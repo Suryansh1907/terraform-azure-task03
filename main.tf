0d70f3d24250a7b08024289c6a2bce2c7c389d87
Suryansh1907 <suryansh_tilak@epam.com>
2025-05-06 23:20:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,49 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rgdetails" {
   name     = var.resource_group_name
   location = var.location
-  tags     = var.tags
+
+  tags = {
+    Creator = "suryansh_tilak@epam.com"
+  }
 }
 
-resource "azurerm_storage_account" "sa" {
+resource "azurerm_storage_account" "sadetails" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rgdetails.name
+  location                 = azurerm_resource_group.rgdetails.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  tags                     = var.tags
+
+  tags = {
+    Creator = "suryansh_tilak@epam.com"
+  }
+
+  depends_on = [azurerm_resource_group.rgdetails]
 }
 
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "vnetdetails" {
   name                = var.vnet_name
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tags
+  resource_group_name = azurerm_resource_group.rgdetails.name
+  location            = azurerm_resource_group.rgdetails.location
+  address_space       = var.address_space
+
+  tags = {
+    Creator = "suryansh_tilak@epam.com"
+  }
+
+  depends_on = [azurerm_resource_group.rgdetails]
 }
 
 resource "azurerm_subnet" "frontend" {
-  name                 = "frontend"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  name                 = var.frontend_subnet_name
+  resource_group_name  = azurerm_resource_group.rgdetails.name
+  virtual_network_name = azurerm_virtual_network.vnetdetails.name
+  address_prefixes     = var.frontend_subnet_prefix
 }
 
 resource "azurerm_subnet" "backend" {
-  name                 = "backend"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  name                 = var.backend_subnet_name
+  resource_group_name  = azurerm_resource_group.rgdetails.name
+  virtual_network_name = azurerm_virtual_network.vnetdetails.name
+  address_prefixes     = var.backend_subnet_prefix
 }
