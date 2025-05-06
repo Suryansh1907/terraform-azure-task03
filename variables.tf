94f37aae61def898ebbc5b4e7720e9eea2d4cd79
Suryansh1907 <suryansh_tilak@epam.com>
2025-05-06 23:44:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,64 +1,21 @@
+
 variable "subscription_id" {
+  description = "Azure Subscription ID"
   type        = string
-  description = "Azure subscription ID"
 }
 
 variable "client_id" {
+  description = "Azure Client ID"
   type        = string
-  description = "Azure client ID"
 }
 
 variable "client_secret" {
+  description = "Azure Client Secret"
   type        = string
-  description = "Azure client secret"
+  sensitive   = true
 }
 
 variable "tenant_id" {
+  description = "Azure Tenant ID"
   type        = string
-  description = "Azure tenant ID"
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "Name of the resource group"
-}
-
-variable "location" {
-  type        = string
-  description = "Azure region"
-}
-
-variable "storage_account_name" {
-  type        = string
-  description = "Name of the storage account"
-}
-
-variable "vnet_name" {
-  type        = string
-  description = "Name of the virtual network"
-}
-
-variable "frontend_subnet_name" {
-  type        = string
-  description = "Name of the frontend subnet"
-}
-
-variable "backend_subnet_name" {
-  type        = string
-  description = "Name of the backend subnet"
-}
-
-variable "address_space" {
-  type        = list(string)
-  description = "Address space for the virtual network"
-}
-
-variable "frontend_subnet_prefix" {
-  type        = list(string)
-  description = "Address prefix for the frontend subnet"
-}
-
-variable "backend_subnet_prefix" {
-  type        = list(string)
-  description = "Address prefix for the backend subnet"
 }
