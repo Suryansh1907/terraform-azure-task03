6ace57cf59624c3b968ede87006fe1d60bd211bb
Suryansh1907 <suryansh_tilak@epam.com>
2025-05-06 23:22:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,64 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure client secret"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID"
+}
 
 variable "resource_group_name" {
-  default = "cmaz-eh8dj90z-mod3-rg"
-}
-
-variable "storage_account_name" {
-  default = "cmazeh8dj90zsa"
-}
-
-variable "vnet_name" {
-  default = "cmaz-eh8dj90z-mod3-vnet"
+  type        = string
+  description = "Name of the resource group"
 }
 
 variable "location" {
-  default = "East US"
+  type        = string
+  description = "Azure region"
 }
 
-variable "tags" {
-  default = {
-    Creator = "suryansh_tilak@epam.com"
-  }
+variable "storage_account_name" {
+  type        = string
+  description = "Name of the storage account"
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "Name of the virtual network"
+}
+
+variable "frontend_subnet_name" {
+  type        = string
+  description = "Name of the frontend subnet"
+}
+
+variable "backend_subnet_name" {
+  type        = string
+  description = "Name of the backend subnet"
+}
+
+variable "address_space" {
+  type        = list(string)
+  description = "Address space for the virtual network"
+}
+
+variable "frontend_subnet_prefix" {
+  type        = list(string)
+  description = "Address prefix for the frontend subnet"
+}
+
+variable "backend_subnet_prefix" {
+  type        = list(string)
+  description = "Address prefix for the backend subnet"
 }
