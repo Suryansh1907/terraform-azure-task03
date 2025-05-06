a0f97f04946d3a78e85bb8944dd62717d49bedc5
Suryansh1907 <suryansh_tilak@epam.com>
2025-05-06 23:45:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,49 +1,9 @@
-resource "azurerm_resource_group" "rgdetails" {
-  name     = var.resource_group_name
-  location = var.location
 
-  tags = {
-    Creator = "suryansh_tilak@epam.com"
-  }
-}
+provider "azurerm" {
+  features {}
 
-resource "azurerm_storage_account" "sadetails" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rgdetails.name
-  location                 = azurerm_resource_group.rgdetails.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    Creator = "suryansh_tilak@epam.com"
-  }
-
-  depends_on = [azurerm_resource_group.rgdetails]
-}
-
-resource "azurerm_virtual_network" "vnetdetails" {
-  name                = var.vnet_name
-  resource_group_name = azurerm_resource_group.rgdetails.name
-  location            = azurerm_resource_group.rgdetails.location
-  address_space       = var.address_space
-
-  tags = {
-    Creator = "suryansh_tilak@epam.com"
-  }
-
-  depends_on = [azurerm_resource_group.rgdetails]
-}
-
-resource "azurerm_subnet" "frontend" {
-  name                 = var.frontend_subnet_name
-  resource_group_name  = azurerm_resource_group.rgdetails.name
-  virtual_network_name = azurerm_virtual_network.vnetdetails.name
-  address_prefixes     = var.frontend_subnet_prefix
-}
-
-resource "azurerm_subnet" "backend" {
-  name                 = var.backend_subnet_name
-  resource_group_name  = azurerm_resource_group.rgdetails.name
-  virtual_network_name = azurerm_virtual_network.vnetdetails.name
-  address_prefixes     = var.backend_subnet_prefix
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
